3057eab79fdd22e83c9d5664afebe805e31b5ca7
Chuang Wang <chuangw.sde@gmail.com>
2021-03-05 09:37:17 -0800

    add defensive checks for imputers

--- eda_utils_py/eda_utils_py.py
+++ eda_utils_py/eda_utils_py.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import altair as alt
 from pandas.api.types import is_numeric_dtype
+import numbers
+
 
 def imputer(dataframe, strategy="mean", fill_value=None):
     """
@@ -18,17 +20,17 @@ def imputer(dataframe, strategy="mean", fill_value=None):
             - If “constant”, then replace missing values with fill_value. Can be used with strings or numeric data.
     fill_value : numerical value, default=None
         When strategy == “constant”, fill_value is used to replace all occurrences of missing_values. If left to the default, fill_value will be 0 when imputing numerical data.
-        
+
     Returns
     -------
     pandas.core.frame.DataFrame
         a dataframe that contains no missing data
-        
+
     Examples
     ---------
     >>> import pandas as pd
     >>> from eda_utils_py import cor_map
-    
+
     >>> data = pd.DataFrame({
     >>>     'SepalLengthCm':[5.1, 4.9, 4.7],
     >>>     'SepalWidthCm':[1.4, 1.4, 1.3],
@@ -41,11 +43,39 @@ def imputer(dataframe, strategy="mean", fill_value=None):
     1            4.9           1.4           0.2
     2            4.7           1.3           0.2
     """
-    pass
+
+    # Tests whether input data is of pd.DataFrame type
+    if not isinstance(dataframe, pd.DataFrame):
+        raise TypeError("The input dataframe must be of pd.DataFrame type")
+
+    # Tests whether input strategy is of type str
+    if not isinstance(strategy, str):
+        raise TypeError("strategy must be of type str")
+
+    # Tests whether input strategy is of type str
+    if not isinstance(fill_value, type(None)) and not isinstance(
+        fill_value, numbers.Number
+    ):
+        raise TypeError("fill_value must be of type None or numeric type")
+
+    # Tests whether the inputs for strategy and fill_value are consistent
+    if isinstance(fill_value, numbers.Number) and strategy != "constant":
+        raise Exception("this not correct")
+
+    # Tests whether the inputs for strategy and fill_value are consistent
+    if isinstance(fill_value, type(None)) and strategy == "constant":
+        raise Exception("this not correct!")
 
 
-def cor_map(dataframe, num_col, col_scheme = 'purpleorange'):
-    
+df = pd.DataFrame(
+    {"col1": [None, 4, 4, 7], "col2": [2, None, None, 2], "col3": [3, None, 6, 6]}
+)
+
+imputer(df, strategy="constant")
+
+
+def cor_map(dataframe, num_col, col_scheme="purpleorange"):
+
     """
     A function to implement a correlation heatmap including coefficients based on given numeric columns of a data frame.
 
@@ -53,12 +83,12 @@ def cor_map(dataframe, num_col, col_scheme = 'purpleorange'):
     ----------
     dataframe : pandas.core.frame.DataFrame
         The data frame to be used for EDA.
-    num_col : list  
+    num_col : list
         A list of string of column names with numeric data from the data frame.
-    col_scheme : str, default = 'purpleorange'  
+    col_scheme : str, default = 'purpleorange'
         The color scheme of the heatmap desired, can only be one of the following;
             - 'purpleorange'
-            - 'blueorange' 
+            - 'blueorange'
             - 'redblue'
 
     Returns
@@ -70,7 +100,7 @@ def cor_map(dataframe, num_col, col_scheme = 'purpleorange'):
     ---------
     >>> import pandas as pd
     >>> from eda_utils_py import cor_map
-    
+
     >>> data = pd.DataFrame({
     >>>     'SepalLengthCm':[5.1, 4.9, 4.7],
     >>>     'SepalWidthCm':[1.4, 1.4, 1.3],
@@ -78,11 +108,11 @@ def cor_map(dataframe, num_col, col_scheme = 'purpleorange'):
     >>>     'Species':['Iris-setosa','Iris-virginica', 'Iris-germanica']
     >>> })
 
-    >>> numerical_columns = ['SepalLengthCm','SepalWidthCm','PetalWidthCm']    
+    >>> numerical_columns = ['SepalLengthCm','SepalWidthCm','PetalWidthCm']
     >>> cor_map(data, numerical_columns, col_scheme = 'purpleorange')
-        
+
     """
-    
+
     # Tests whether input data is of pd.DataFrame type
     if not isinstance(dataframe, pd.DataFrame):
         raise TypeError("The input dataframe must be of pd.DataFrame type")
@@ -90,53 +120,53 @@ def cor_map(dataframe, num_col, col_scheme = 'purpleorange'):
     # Tests whether input num_col is of type list
     if not isinstance(num_col, list):
         raise TypeError("The input num_col must be of type list")
-    
-    # Tests whether values of num_col is of type str 
+
+    # Tests whether values of num_col is of type str
     for x in num_col:
         if not isinstance(x, str):
             raise TypeError("The type of values in num_col must all be str")
 
-    # Tests whether input col_scheme is of type str    
+    # Tests whether input col_scheme is of type str
     if not isinstance(col_scheme, str):
         raise TypeError("col_scheme must be of type str")
 
-    # Tests whether col_scheme is one of three possible options    
-    if col_scheme not in ('purpleorange', 'blueorange', 'redblue'):
-        raise Exception("This color scheme is not available, please use either 'purpleorange', 'blueorange' or 'redblue'")
+    # Tests whether col_scheme is one of three possible options
+    if col_scheme not in ("purpleorange", "blueorange", "redblue"):
+        raise Exception(
+            "This color scheme is not available, please use either 'purpleorange', 'blueorange' or 'redblue'"
+        )
 
     # Tests whether all input columns exist in the input data
     for x in num_col:
         if x not in list(dataframe.columns):
-            raise Exception("The given column names must exist in the given dataframe.")    
-        
+            raise Exception("The given column names must exist in the given dataframe.")
+
     # Tests whether all input columns in num_col are numeric columns
     for x in num_col:
         if not is_numeric_dtype(dataframe[x]):
             raise Exception("The given numerical columns must all be numeric.")
-    
-    
-    corr_matrix = dataframe[num_col].corr().reset_index().melt('index')
-    corr_matrix.columns = ['var1', 'var2', 'cor']
 
-    plot = alt.Chart(corr_matrix).mark_rect().encode(
-        x=alt.X('var1', title=None),
-        y=alt.Y('var2', title=None),
-        color=alt.Color('cor',legend=None,
-                       scale = alt.Scale(scheme = col_scheme)),
-    ).properties(
-        title = 'Correlation Matrix',
-        width=400, height=400
+    corr_matrix = dataframe[num_col].corr().reset_index().melt("index")
+    corr_matrix.columns = ["var1", "var2", "cor"]
+
+    plot = (
+        alt.Chart(corr_matrix)
+        .mark_rect()
+        .encode(
+            x=alt.X("var1", title=None),
+            y=alt.Y("var2", title=None),
+            color=alt.Color("cor", legend=None, scale=alt.Scale(scheme=col_scheme)),
+        )
+        .properties(title="Correlation Matrix", width=400, height=400)
     )
 
     text = plot.mark_text(size=15).encode(
-        text=alt.Text('cor', format=".2f"),
+        text=alt.Text("cor", format=".2f"),
         color=alt.condition(
-            "datum.cor > 0.5 | datum.cor < -0.3",
-            alt.value('white'),
-            alt.value('black')
-        )
+            "datum.cor > 0.5 | datum.cor < -0.3", alt.value("white"), alt.value("black")
+        ),
     )
-    
+
     cor_heatmap = plot + text
 
     return cor_heatmap
@@ -147,26 +177,26 @@ def outlier_identifier(dataframe, columns=None, method="trim"):
     A function that identify and deal with outliers based on the method the user choose
 
     Parameters
-    ---------- 
+    ----------
     dataframe : pandas.core.frame.DataFrame
         The target dataframe where the function is performed.
     columns : list, default=None
         The target columns where the function needed to be performed. Defualt is None, the function will check all columns
     method : string
-        The method of dealing with outliers. 
+        The method of dealing with outliers.
             - if "trim" : we completely remove data points that are outliers.
             - if "median" : we replace outliers with median values
-        
+
     Returns
     -------
     pandas.core.frame.DataFrame
         a dataframe which the outlier has already process by the chosen method
-    
+
     Examples
     --------
     >>> import pandas as pd
     >>> from eda_utils_py import cor_map
-        
+
     >>> data = pd.DataFrame({
     >>>    'SepalLengthCm':[5.1, 4.9, 4.7],
     >>>    'SepalWidthCm':[1.4, 1.4, 9999999.99],
@@ -200,7 +230,7 @@ def scale(dataframe, columns=None):
     --------
     >>> import pandas as pd
     >>> from eda_utils_py import scale
-    
+
     >>> data = pd.DataFrame({
     >>>     'SepalLengthCm':[5.1, 4.9, 4.7],
     >>>     'SepalWidthCm':[1.4, 1.4, 1.3],
@@ -209,7 +239,7 @@ def scale(dataframe, columns=None):
     >>> })
 
     >>> numerical_columns = ['SepalLengthCm','SepalWidthCm','PetalWidthCm']
-    
+
     >>> scale(data, numerical_columns)
     """
     pass
