182d429b056a3853975a9f5137f48df3e5a694c0
fatse <fatimeseelimi@gmail.com>
2021-03-05 12:03:58 -0800

    Move utility functions for scaler to another file

--- eda_utils_py/eda_utils_py.py
+++ eda_utils_py/eda_utils_py.py
@@ -3,6 +3,10 @@ import altair as alt
 from pandas.api.types import is_numeric_dtype
 import numbers
 
+from eda_utils_py.utils import _standardize_dataset, _dataset_minmax, _minmax_dataset, _calculate_means, \
+    _calculate_stdevs
+
+
 
 def imputer(df, strategy="mean", fill_value=None):
     """
@@ -304,48 +308,3 @@ def scale(dataframe, columns=None, scaler="standard"):
     return scaler
 
 
-# calculate means
-def _calculate_means(dataset):
-    means = [0 for _ in range(len(dataset[0]))]
-    for i in range(len(dataset[0])):
-        col_values = [row[i] for row in dataset]
-        means[i] = sum(col_values) / float(len(dataset))
-
-    return means
-
-
-# calculate standard deviation
-def _calculate_stdevs(dataset, means):
-    stdevs = [0 for _ in range(len(dataset[0]))]
-    for i in range(len(dataset[0])):
-        variance = [pow(row[i] - means[i], 2) for row in dataset]
-        stdevs[i] = sum(variance)
-    stdevs = [sqrt(x / float(len(dataset) - 1)) for x in stdevs]
-
-    return stdevs
-
-
-# scaled_value = (value - mean) / stdev
-def _standardize_dataset(dataset, means, stdevs):
-    for row in dataset:
-        for i in range(len(row)):
-            row[i] = (row[i] - means[i]) / stdevs[i]
-
-
-# Find the min and max values for each column
-def _dataset_minmax(dataset):
-    minmax = list()
-    for i in range(len(dataset[0])):
-        col_values = [row[i] for row in dataset]
-        value_min = min(col_values)
-        value_max = max(col_values)
-        minmax.append([value_min, value_max])
-    return minmax
-
-
-# Rescale dataset columns to the range 0-1
-# scaled_value = (value - min) / (mix - min)
-def _minmax_dataset(dataset, minmax):
-    for row in dataset:
-        for i in range(len(row)):
-            row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
