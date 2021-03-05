d3b2e166d7576e051b90b40ee31f3176852cfe72
fatse <fatimeseelimi@gmail.com>
2021-03-05 12:21:34 -0800

    Add docstrings for minmax utility functions

--- eda_utils_py/utils.py
+++ eda_utils_py/utils.py
@@ -2,47 +2,76 @@ from math import sqrt
 
 
 # calculate means
-def _calculate_means(dataset):
-    means = [0 for _ in range(len(dataset[0]))]
-    for i in range(len(dataset[0])):
-        col_values = [row[i] for row in dataset]
-        means[i] = sum(col_values) / float(len(dataset))
+def _calculate_means(dataframe):
+    means = [0 for _ in range(len(dataframe[0]))]
+    for i in range(len(dataframe[0])):
+        col_values = [row[i] for row in dataframe]
+        means[i] = sum(col_values) / float(len(dataframe))
 
     return means
 
 
 # calculate standard deviation
-def _calculate_stdevs(dataset, means):
-    stdevs = [0 for _ in range(len(dataset[0]))]
-    for i in range(len(dataset[0])):
-        variance = [pow(row[i] - means[i], 2) for row in dataset]
+def _calculate_stdevs(dataframe, means):
+    stdevs = [0 for _ in range(len(dataframe[0]))]
+    for i in range(len(dataframe[0])):
+        variance = [pow(row[i] - means[i], 2) for row in dataframe]
         stdevs[i] = sum(variance)
-    stdevs = [sqrt(x / float(len(dataset) - 1)) for x in stdevs]
+    stdevs = [sqrt(x / float(len(dataframe) - 1)) for x in stdevs]
 
     return stdevs
 
 
 # scaled_value = (value - mean) / stdev
-def _standardize_dataset(dataset, means, stdevs):
-    for row in dataset:
+def _standardize_dataset(dataframe, means, stdevs):
+    for row in dataframe:
         for i in range(len(row)):
             row[i] = (row[i] - means[i]) / stdevs[i]
 
 
-# Find the min and max values for each column
-def _dataset_minmax(dataset):
+def _get_dataset_minmax(dataframe):
+    """Compute the minimum and maximum for each feature in the dataset, to be used for later scaling.
+    Parameters
+    ----------
+    dataframe : pandas.DataFrame
+        The data frame to be used for EDA.
+    Returns
+    -------
+    minmax : list
+        An array of minimum and maximum values
+    """
     minmax = list()
-    for i in range(len(dataset[0])):
-        col_values = [row[i] for row in dataset]
+    for i in range(len(dataframe[0])):
+        col_values = [row[i] for row in dataframe]
         value_min = min(col_values)
         value_max = max(col_values)
         minmax.append([value_min, value_max])
     return minmax
 
 
-# Rescale dataset columns to the range 0-1
-# scaled_value = (value - min) / (mix - min)
-def _minmax_dataset(dataset, minmax):
-    for row in dataset:
+def _minmax_dataset(dataframe, minmax):
+    """Transform features by rescaling each feature to the range between 0 and 1.
+    The transformation is given by:
+
+        scaled_value = (feature_value - min) / (mix - min)
+
+    where min, max = feature_range.
+    
+    This transformation is often used as an alternative to zero mean,
+    unit variance scaling.
+    
+    Parameters
+    ----------
+    dataframe : pandas.DataFrame
+        The data frame to be used for EDA.
+    minmax : object
+        The array returned from def _get_dataset_minmax(dataset)
+    Returns
+    -------
+    self : object
+        Scaled dataset
+    """
+    minmax = _get_dataset_minmax(dataframe)
+    for row in dataframe:
         for i in range(len(row)):
             row[i] = (row[i] - minmax[i][0]) / (minmax[i][1] - minmax[i][0])
