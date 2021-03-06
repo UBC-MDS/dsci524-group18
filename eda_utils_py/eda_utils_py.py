67363ce18a9234fc1bdcad3fb578098daf1ea6e4
Jiacheng <wangjc640@gmail.com>
2021-03-05 23:51:15 -0500

    add my function

--- eda_utils_py/eda_utils_py.py
+++ eda_utils_py/eda_utils_py.py
@@ -1,52 +1,90 @@
-import pandas as pd
 import altair as alt
+import pandas as pd
 from pandas.api.types import is_numeric_dtype
+import numbers
 import numpy as np
 
 
-def imputer(dataframe, strategy="mean", fill_value=None):
+def imputer(df, strategy="mean", fill_value=None):
     """
     A function to implement imputation functionality for completing missing values.
 
     Parameters
     ----------
-    dataframe : pandas.core.frame.DataFrame
+    df : pandas.core.frame.DataFrame
         a dataframe that might contain missing data
     strategy : string, default="mean"
         The imputation strategy.
-            - If “mean”, then replace missing values using the mean along each column. Can only be used with numeric data.
-            - If “median”, then replace missing values using the median along each column. Can only be used with numeric data.
-            - If “most_frequent”, then replace missing using the most frequent value along each column. Can be used with strings or numeric data. If there is more than one such value, only the smallest is returned.
-            - If “constant”, then replace missing values with fill_value. Can be used with strings or numeric data.
-    fill_value : string or numerical value, default=None
-        When strategy == “constant”, fill_value is used to replace all occurrences of missing_values. If left to the default, fill_value will be 0 when imputing numerical data and “missing_value” for strings or object data types.
-        
+            - If "mean", then replace missing values using the mean along each column. Can only be used with numeric data.
+            - If "median", then replace missing values using the median along each column. Can only be used with numeric data.
+            - If "most_frequent", then replace missing using the most frequent value along each column. Can be used with strings or numeric data. If there is more than one such value, only the smallest is returned.
+            - If "constant", then replace missing values with fill_value. Can be used with strings or numeric data.
+    fill_value : numerical value, default=None
+        When strategy == "constant", fill_value is used to replace all occurrences of missing_values. If left to the default, fill_value will be 0 when imputing numerical data.
+
     Returns
     -------
     pandas.core.frame.DataFrame
         a dataframe that contains no missing data
-        
+
     Examples
     ---------
-    >>> import pandas as pd
-    >>> from eda_utils_py import cor_map
-    
-    >>> data = pd.DataFrame({
-    >>>     'SepalLengthCm':[5.1, 4.9, 4.7],
-    >>>     'SepalWidthCm':[1.4, 1.4, 1.3],
-    >>>     'PetalWidthCm':[0.2, None, 0.2]
-    >>> })
+    >> import pandas as pd
+    >> from eda_utils_py import cor_map
 
-    >>> imputer(data, numerical_columns)
+    >> data = pd.DataFrame({
+    >>     'SepalLengthCm':[5.1, 4.9, 4.7],
+    >>     'SepalWidthCm':[1.4, 1.4, 1.3],
+    >>     'PetalWidthCm':[0.2, None, 0.2]
+    >> })
+
+    >> imputer(data, numerical_columns)
        SepalLengthCm  SepalWidthCm  PetalWidthCm
     0            5.1           1.4           0.2
     1            4.9           1.4           0.2
     2            4.7           1.3           0.2
     """
-    pass
+
+    # Tests whether input data is of pd.DataFrame type
+    if not isinstance(df, pd.DataFrame):
+        raise TypeError("The input dataframe must be of pd.DataFrame type")
+
+    # Tests whether input strategy is of type str
+    if not isinstance(strategy, str):
+        raise TypeError("strategy must be of type str")
+
+    # Tests whether input fill_value is of type numbers or None
+    if not isinstance(fill_value, type(None)) and not isinstance(
+            fill_value, numbers.Number
+    ):
+        raise TypeError("fill_value must be of type None or numeric type")
+
+    # Tests whether the inputs for strategy and fill_value are consistent
+    if isinstance(fill_value, numbers.Number) and strategy != "constant":
+        raise Exception("fill_value can be a number only if strategy is 'constant'")
+
+    # Tests whether the inputs for strategy and fill_value are consistent
+    if isinstance(fill_value, type(None)) and strategy == "constant":
+        raise Exception("fill_value should be a number when strategy is 'constant'")
+
+    result = pd.DataFrame()
+    if strategy == "mean":
+        result = df.apply(lambda x: x.fillna(x.mean()), axis=0)
+    elif strategy == "median":
+        result = df.apply(lambda x: x.fillna(x.median()), axis=0)
+    elif strategy == "most_frequent":
+        result = df.apply(lambda x: x.fillna(x.value_counts().index[0]), axis=0)
+    elif strategy == "constant":
+        result = df.apply(lambda x: x.fillna(fill_value))
+    else:
+        raise Exception(
+            "strategy should be one of 'mean', 'median', 'most_frequent' and 'constant'"
+        )
+
+    return result
 
 
-def cor_map(dataframe, num_col):
+def cor_map(dataframe, num_col, col_scheme="purpleorange"):
     """
     A function to implement a correlation heatmap including coefficients based on given numeric columns of a data frame.
 
@@ -54,8 +92,13 @@ def cor_map(dataframe, num_col):
     ----------
     dataframe : pandas.core.frame.DataFrame
         The data frame to be used for EDA.
-    num_col : list  
+    num_col : list
         A list of string of column names with numeric data from the data frame.
+    col_scheme : str, default = 'purpleorange'
+        The color scheme of the heatmap desired, can only be one of the following;
+            - 'purpleorange'
+            - 'blueorange'
+            - 'redblue'
 
     Returns
     -------
@@ -64,21 +107,77 @@ def cor_map(dataframe, num_col):
 
     Examples
     ---------
-    >>> import pandas as pd
-    >>> from eda_utils_py import cor_map
-    
-    >>> data = pd.DataFrame({
-    >>>     'SepalLengthCm':[5.1, 4.9, 4.7],
-    >>>     'SepalWidthCm':[1.4, 1.4, 1.3],
-    >>>     'PetalWidthCm':[0.2, 0.2, 0.2],
-    >>>     'Species':['Iris-setosa','Iris-virginica', 'Iris-germanica']
-    >>> })
+    >> import pandas as pd
+    >> from eda_utils_py import cor_map
 
-    >>> numerical_columns = ['SepalLengthCm','SepalWidthCm','PetalWidthCm']    
-    >>> cor_map(data, numerical_columns)
-        
+    >> data = pd.DataFrame({
+    >>     'SepalLengthCm':[5.1, 4.9, 4.7],
+    >>     'SepalWidthCm':[1.4, 1.4, 1.3],
+    >>     'PetalWidthCm':[0.2, 0.2, 0.2],
+    >>     'Species':['Iris-setosa','Iris-virginica', 'Iris-germanica']
+    >> })
+
+    >> numerical_columns = ['SepalLengthCm','SepalWidthCm','PetalWidthCm']
+    >> cor_map(data, numerical_columns, col_scheme = 'purpleorange')
     """
-    pass
+
+    # Tests whether input data is of pd.DataFrame type
+    if not isinstance(dataframe, pd.DataFrame):
+        raise TypeError("The input dataframe must be of pd.DataFrame type")
+
+    # Tests whether input num_col is of type list
+    if not isinstance(num_col, list):
+        raise TypeError("The input num_col must be of type list")
+
+    # Tests whether values of num_col is of type str
+    for x in num_col:
+        if not isinstance(x, str):
+            raise TypeError("The type of values in num_col must all be str")
+
+    # Tests whether input col_scheme is of type str
+    if not isinstance(col_scheme, str):
+        raise TypeError("col_scheme must be of type str")
+
+    # Tests whether col_scheme is one of three possible options
+    if col_scheme not in ("purpleorange", "blueorange", "redblue"):
+        raise Exception(
+            "This color scheme is not available, please use either 'purpleorange', 'blueorange' or 'redblue'"
+        )
+
+    # Tests whether all input columns exist in the input data
+    for x in num_col:
+        if x not in list(dataframe.columns):
+            raise Exception("The given column names must exist in the given dataframe.")
+
+    # Tests whether all input columns in num_col are numeric columns
+    for x in num_col:
+        if not is_numeric_dtype(dataframe[x]):
+            raise Exception("The given numerical columns must all be numeric.")
+
+    corr_matrix = dataframe[num_col].corr().reset_index().melt("index")
+    corr_matrix.columns = ["var1", "var2", "cor"]
+
+    plot = (
+        alt.Chart(corr_matrix)
+            .mark_rect()
+            .encode(
+            x=alt.X("var1", title=None),
+            y=alt.Y("var2", title=None),
+            color=alt.Color("cor", legend=None, scale=alt.Scale(scheme=col_scheme)),
+        )
+            .properties(title="Correlation Matrix", width=400, height=400)
+    )
+
+    text = plot.mark_text(size=15).encode(
+        text=alt.Text("cor", format=".2f"),
+        color=alt.condition(
+            "datum.cor > 0.5 | datum.cor < -0.3", alt.value("white"), alt.value("black")
+        ),
+    )
+
+    cor_heatmap = plot + text
+
+    return cor_heatmap
 
 
 def outlier_identifier(dataframe, columns=None, method="trim"):
@@ -174,9 +273,9 @@ def outlier_identifier(dataframe, columns=None, method="trim"):
     return df
 
 
-def scale(dataframe, columns=None):
+def scale(dataframe, columns=None, scaler="standard"):
     """
-    A function to scale features by removing the mean and scaling to unit variance
+    A function to scale features either by using standard scaler or minmax scaler method
 
     Parameters
     ----------
@@ -184,7 +283,11 @@ def scale(dataframe, columns=None):
         The data frame to be used for EDA.
     columns : list, default=None
         A list of string of column names with numeric data from the data frame that we wish to scale.
-
+    scaler: str, default="standard"
+        A string to specify the sclaing method to be used
+            - if "standard": it transforms features by centering the distribution of the data on the value 0 and the standard
+                        deviation to the value 1.
+            - if "minmax": it transforms features by rescaling each feature to the range between 0 and 1.
     Returns
     -------
     dataframe : pandas.core.frame.DataFrame
@@ -192,18 +295,116 @@ def scale(dataframe, columns=None):
 
     Examples
     --------
-    >>> import pandas as pd
-    >>> from eda_utils_py import scale
-    
-    >>> data = pd.DataFrame({
-    >>>     'SepalLengthCm':[5.1, 4.9, 4.7],
-    >>>     'SepalWidthCm':[1.4, 1.4, 1.3],
-    >>>     'PetalWidthCm:[0.2, 0.2, 0.2],
-    >>>     'Species':['Iris-setosa','Iris-virginica', 'Iris-germanica']
-    >>> })
+    >> import pandas as pd
+    >> from eda_utils_py import scale
 
-    >>> numerical_columns = ['SepalLengthCm','SepalWidthCm','PetalWidthCm']
-    
-    >>> scale(data, numerical_columns)
+    >> data = pd.DataFrame({
+    >>     'SepalLengthCm':[5.1, 4.9, 4.7],
+    >>     'SepalWidthCm':[1.4, 1.4, 1.3],
+    >>     'PetalWidthCm:[0.2, 0.2, 0.2],
+    >>     'Species':['Iris-setosa','Iris-virginica', 'Iris-germanica']
+    >> })
+
+    >> numerical_columns = ['SepalLengthCm','SepalWidthCm','PetalWidthCm']
+
+    >> scale(data, numerical_columns, scaler="minmax")
     """
-    pass
+
+    # Check if input data is of pd.DataFrame type
+    if not isinstance(dataframe, pd.DataFrame):
+        raise TypeError("The input dataframe must be of pd.DataFrame type")
+
+    # Check if input num_col is of type list
+    if not isinstance(columns, list):
+        raise TypeError("The input columns must be of type list")
+
+    # Check if values of columns are of type str
+    for col in columns:
+        if not isinstance(col, str):
+            raise TypeError("The name of features in columns list must all be str")
+
+    # Check if all input columns exist in the input data
+    for col in columns:
+        if col not in list(dataframe.columns):
+            raise Exception("The given column names must exist in the given dataframe.")
+
+    # Check if all input columns in num_col are numeric columns
+    for col in columns:
+        if not is_numeric_dtype(dataframe[col]):
+            raise Exception("The given numerical columns must all be numeric.")
+
+    # Check if scaler is of type str
+    if not isinstance(scaler, str):
+        raise TypeError("Scaler must be of type str")
+
+    # Check if all input columns exist in the input data
+    for col in columns:
+        if col not in list(dataframe.columns):
+            raise Exception("The given column names must exist in the given dataframe.")
+
+    # Check if all input columns in num_col are numeric columns
+    for col in columns:
+        if not is_numeric_dtype(dataframe[col]):
+            raise Exception("The given columns must all be numeric.")
+
+    scaled_df = None
+    if scaler == "minmax":
+        scaled_df = _minmax(dataframe[columns])
+    else:
+        scaled_df = _standardize(dataframe[columns])
+
+    return scaled_df
+
+
+def _standardize(dataframe):
+    """Transform features by centering the distribution of the data
+    on the value 0 and the standard deviation to the value 1.
+
+    The transformation is given by:
+
+        scaled_value = (value - mean) / standard deviation
+
+    Parameters
+    ----------
+    dataframe : pandas.DataFrame
+        The data frame to be used for EDA.
+    Returns
+    -------
+    self : object
+        Scaled dataset
+    """
+    res = dataframe.copy()
+    for feature_name in dataframe.columns:
+        mean = dataframe[feature_name].mean()
+        stdev = dataframe[feature_name].std()
+        res[feature_name] = (dataframe[feature_name] - mean) / stdev
+    return res
+
+
+def _minmax(dataframe):
+    """Transform features by rescaling each feature to the range between 0 and 1.
+        The transformation is given by:
+
+            scaled_value = (feature_value - min) / (mix - min)
+
+        where min, max = feature_range.
+
+        This transformation is often used as an alternative to zero mean,
+        unit variance scaling.
+
+        Parameters
+        ----------
+        dataframe : pandas.DataFrame
+            The data frame to be used for EDA.
+        Returns
+        -------
+        self : object
+            Scaled dataset
+        """
+
+    res = dataframe.copy()
+    for feature_name in dataframe.columns:
+        max = dataframe[feature_name].max()
+        min = dataframe[feature_name].min()
+        res[feature_name] = (dataframe[feature_name] - min) / (max - min)
+    return res
--- tests/test_eda_utils_py.py
+++ tests/test_eda_utils_py.py
@@ -1,13 +1,244 @@
 from eda_utils_py import __version__
 from eda_utils_py import eda_utils_py
-import pandas as pd
-import altair as alt
-from pandas.api.types import is_numeric_dtype
-import numpy as np
 from pytest import raises
+import pandas as pd
+from pandas._testing import assert_frame_equal
+import numpy as np
+
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_imputer():
+    data = pd.DataFrame(
+        {"col1": [None, 4, 4, 7], "col2": [2, None, None, 2], "col3": [3, None, 6, 6]}
+    )
+
+    imp_mean = pd.DataFrame(
+        {
+            "col1": [5.0, 4.0, 4.0, 7.0],
+            "col2": [2.0, 2.0, 2.0, 2.0],
+            "col3": [3.0, 5.0, 6.0, 6.0],
+        }
+    )
+
+    imp_median = pd.DataFrame(
+        {
+            "col1": [4.0, 4.0, 4.0, 7.0],
+            "col2": [2.0, 2.0, 2.0, 2.0],
+            "col3": [3.0, 6.0, 6.0, 6.0],
+        }
+    )
+
+    imp_most_frequent = pd.DataFrame(
+        {
+            "col1": [4.0, 4.0, 4.0, 7.0],
+            "col2": [2.0, 2.0, 2.0, 2.0],
+            "col3": [3.0, 6.0, 6.0, 6.0],
+        }
+    )
+
+    imp_constant = pd.DataFrame(
+        {
+            "col1": [1.0, 4.0, 4.0, 7.0],
+            "col2": [2.0, 1.0, 1.0, 2.0],
+            "col3": [3.0, 1.0, 6.0, 6.0],
+        }
+    )
+
+    # Tests whether data is not of dataframe raises TypeError
+    with raises(TypeError):
+        eda_utils_py.imputer([4, None, 4, 7])
+
+    # Tests whether strategy of incorrect type raises TypeError
+    with raises(TypeError):
+        eda_utils_py.imputer(data, strategy=2)
+
+    # Tests whether fill_value of incorrect type raises TypeError
+    with raises(TypeError):
+        eda_utils_py.imputer(data, strategy="constant", fill_value="a string")
+
+    # Tests whether inconsistency between strategy and fill_value raises Exception
+    with raises(Exception):
+        eda_utils_py.imputer(data, strategy="constant", fill_value=None)
+
+    # Tests whether inconsistency between strategy and fill_value raises Exception
+    with raises(Exception):
+        eda_utils_py.imputer(data, strategy="median", fill_value=3)
+
+    assert pd.DataFrame.equals(
+        eda_utils_py.imputer(data), imp_mean
+    ), "The returned dataframe using mean inputer is not correct"
+    assert pd.DataFrame.equals(
+        eda_utils_py.imputer(data, "median"), imp_median
+    ), "The returned dataframe using median inputer is not correct"
+    assert pd.DataFrame.equals(
+        eda_utils_py.imputer(data, "most_frequent"), imp_most_frequent
+    ), "The returned dataframe using most_frequent inputer is not correct"
+    assert pd.DataFrame.equals(
+        eda_utils_py.imputer(data, "constant", 1), imp_constant
+    ), "The returned dataframe using constant imputer is not correct"
+
+
+def test_cor_map():
+    """
+    A function to test whether the output of cor_map() is correct.
+    """
+
+    data = pd.DataFrame(
+        {
+            "SepalLengthCm": [5.1, 4.9, 4.7],
+            "SepalWidthCm": [1.4, 1.4, 1.3],
+            "PetalWidthCm": [0.2, 0.1, 0.2],
+            "Species": ["Iris-setosa", "Iris-virginica", "Iris-germanica"],
+        }
+    )
+
+    num_col_test = ["SepalLengthCm", "SepalWidthCm", "PetalWidthCm"]
+
+    plot = eda_utils_py.cor_map(data, num_col_test, "redblue")
+
+    # Tests whether output is of Altair type
+    assert "altair" in str(type(plot)), "Output is not of Altair type"
+
+    # Tests whether or not there are NaNs produced in the correlation values
+    assert (
+            plot.data["cor"].isnull().sum() == 0
+    ), "There are NaN produced as correlation values"
+
+    # Tests whether plot output scheme is one of the three given color schemes
+    plot_dict = plot.to_dict()
+    assert plot_dict["layer"][0]["encoding"]["color"]["scale"]["scheme"] in (
+        "purpleorange",
+        "blueorange",
+        "redblue",
+    ), "The plot color scheme is not one of the expected schemes"
+
+    # Tests whether heatmap portion of plot is mark_rect()
+    assert plot_dict["layer"][0]["mark"] == "rect", "mark should be rect"
+
+    # Tests whether heatmap and correlation values have the same referenced var column
+    assert (
+            plot_dict["layer"][0]["encoding"]["x"]["field"]
+            == plot_dict["layer"][1]["encoding"]["x"]["field"]
+    ), "The heatmap and the correlation values are not referring to the same corresponding underlying variable x"
+    assert (
+            plot_dict["layer"][0]["encoding"]["y"]["field"]
+            == plot_dict["layer"][1]["encoding"]["y"]["field"]
+    ), "The heatmap and the correlation values are not referring to the same corresponding underlying variable y"
+
+    # Tests whether axes is using correct calculated var column as reference
+    assert (
+            plot_dict["layer"][0]["encoding"]["x"]["field"] == "var1"
+    ), "x should be referring to var1"
+    assert (
+            plot_dict["layer"][0]["encoding"]["y"]["field"] == "var2"
+    ), "y should be referring to var2"
+
+    # Testing the Exception Errors
+    data2 = data.copy().to_csv()
+
+    num_col_test1 = (1, 2, 3, 4)
+    num_col_test2 = [1, 2, 3, "SepalLengthCm"]
+    num_col_test3 = ["hi", "hey", "hi"]
+    num_col_test4 = ["SepalLengthCm", "SepalWidthCm", "PetalWidthCm", "Species"]
+    col_scheme_test = 3
+
+    # Tests whether data is not of dataframe raises TypeError
+    with raises(TypeError):
+        eda_utils_py.cor_map(data2, num_col_test)
+
+    # Tests whether num_col is of not type list raises TypeError
+    with raises(TypeError):
+        eda_utils_py.cor_map(data, num_col_test1)
+
+    # Tests whether contents of num_col is notof type str raises TypeError
+    with raises(TypeError):
+        eda_utils_py.cor_map(data, num_col_test2)
+
+    # Tests whether inputting unallowed col_scheme raises Exception
+    with raises(Exception):
+        eda_utils_py.cor_map(data, num_col_test, "bluegreen")
+
+    # Tests whether inputting unallowed col_scheme raises TypeError
+    with raises(TypeError):
+        eda_utils_py.cor_map(data, num_col_test, col_scheme_test)
+
+    # Tests whether columns do not exist in raises Exception
+    with raises(Exception):
+        eda_utils_py.cor_map(data, num_col_test3)
+
+    # Tests whether if not all columns in num_col is numeric raises Exception
+    with raises(Exception):
+        eda_utils_py.cor_map(data, num_col_test4)
+
+
+def test_scaler():
+    mock_df_1 = pd.DataFrame(
+        {"col1": [1, 0, 0, 3, 4],
+         "col2": [4, 1, 1, 0, 1],
+         "col3": [2, 0, 0, 2, 1]}
+    )
+
+    mock_df_2 = pd.DataFrame(
+        {"col1": [1, 2, 1],
+         "col2": [0, 1, 2]}
+    )
+
+    mock_df_1_standard = pd.DataFrame(
+        {"col1": [-0.3302891295379082, -0.8807710121010884, -0.8807710121010884, 0.7706746355884523, 1.3211565181516325],
+         "col2": [1.714389230829046, -0.26375218935831474, -0.26375218935831474, -0.9231326627541017, -0.26375218935831474],
+         "col3": [1.0, -1.0, -1.0, 1.0, 0.0]}
+    )
+
+    mock_df_1_minmax = pd.DataFrame(
+        {"col1": [0.25, 0.00, 0.00, 0.75, 1.00],
+         "col2": [1.00, 0.25, 0.25, 0.00, 0.25],
+         "col3": [1.0, 0.0, 0.0, 1.0, 0.5]}
+    )
+
+    mock_df_2_standard = pd.DataFrame(
+        {"col1": [-0.5773502691896256, 1.1547005383792517, -0.5773502691896256],
+         "col2": [-1.0, 0.0, 1.0]}
+    )
+
+    mock_df_2_minmax = pd.DataFrame(
+        {"col1": [0.0, 1.0, 0.0],
+         "col2": [0.0, 0.5, 1.0]}
+    )
+
+    standard_scaled_mock_df_1 = eda_utils_py.scale(mock_df_1, ['col1', 'col2', 'col3'])
+    standard_scaled_mock_df_2 = eda_utils_py.scale(mock_df_2, ['col1', 'col2'])
+    minmax_scaled_mock_df_1 = eda_utils_py.scale(mock_df_1, ['col1', 'col2', 'col3'], scaler="minmax")
+    minmax_scaled_mock_df_2 = eda_utils_py.scale(mock_df_2, ['col1', 'col2'], scaler="minmax")
+
+    # Tests whether data is not of type pd.Dataframe raises TypeError
+    with raises(TypeError):
+        eda_utils_py.scale([14, None, 3, 27])
+
+    # Tests whether scaler of incorrect method raises TypeError
+    with raises(TypeError):
+        eda_utils_py.scale(mock_df_1, ['col1', 'col2'], scaler=1)
+
+    # Tests whether columns of incorrect type raises TypeError
+    with raises(TypeError):
+        eda_utils_py.scale(mock_df_1, {'col1': 1, 'col2': 3})
+
+    assert pd.DataFrame.equals(
+        standard_scaled_mock_df_1, mock_df_1_standard
+    ), "The returned dataframe using standard scaler method is not correct"
+    assert pd.DataFrame.equals(
+        minmax_scaled_mock_df_1, mock_df_1_minmax
+    ), "The returned dataframe using standard scaler method is not correct"
+
+
+    assert pd.DataFrame.equals(
+        standard_scaled_mock_df_2, mock_df_2_standard
+    ), "The returned dataframe using most_frequent inputer is not correct"
+    assert pd.DataFrame.equals(
+        minmax_scaled_mock_df_2, mock_df_2_minmax
+    ), "The returned dataframe using constant imputer is not correct"
 
 
 def test_outlier_identifier():
