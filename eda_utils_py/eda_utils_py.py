3480f2027c5454ab7027bce00b014efe442986df
Jiacheng <wangjc640@gmail.com>
2021-03-13 21:59:38 -0500

    add example result

--- eda_utils_py/eda_utils_py.py
+++ eda_utils_py/eda_utils_py.py
@@ -207,15 +207,22 @@ def outlier_identifier(dataframe, columns=None, method="trim"):
     >> import pandas as pd
     >> from eda_utils_py import cor_map
         
-    >> data = pd.DataFrame({
-    >>    'SepalLengthCm':[5.1, 4.9, 4.7],
-    >>    'SepalWidthCm':[1.4, 1.4, 99],
-    >>    'PetalWidthCm:[0.2, 0.2, 0.2],
-    >>    'Species':['Iris-setosa', 'Iris-virginica', 'Iris-germanica']
-    >> })
+    >> df = pd.DataFrame({
+    >>    'SepalLengthCm' : [5.1, 4.9, 4.7, 5.5, 5.1, 50, 5.4, 5.0, 5.2, 5.3, 5.1],
+    >>    'SepalWidthCm' : [1.4, 1.4, 20, 2.0, 0.7, 1.6, 1.2, 1.4, 1.8, 1.5, 2.1],
+    >>    'PetalWidthCm' : [0.2, 0.2, 0.2, 0.3, 0.4, 0.5, 0.5, 0.6, 0.4, 0.2, 5]
+    >>})
 
     >> outlier_identifier(data)
-
+    >> 	 SepalLengthCm  	SepalWidthCm	   PetalWidthCm
+    >> 0	5.1	                1.4	                0.2
+    >> 1	4.9	                1.4	                0.2
+    >> 2	5.5	                2.0	                0.3
+    >> 3	5.1	                0.7	                0.4
+    >> 4	5.4	                1.2             	0.5
+    >> 5	5.0	                1.4	                0.6
+    >> 6	5.2	                1.8	                0.4
+    >> 7	5.3	                1.5	                0.2
 
     """
     if not isinstance(dataframe, pd.DataFrame):
